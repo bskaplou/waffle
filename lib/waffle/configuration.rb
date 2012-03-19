74466db6c9fa34ef2b47b95b870c84df1c4c0da9
Alexander Lomakin <alexander.lomakin@gmail.com>
2012-03-19 22:08:44 +0400

    Added Rails config parsing when in Rails environment. Added Utils test

--- lib/waffle/configuration.rb
+++ lib/waffle/configuration.rb
@@ -1,8 +1,16 @@
+require 'yaml'
+
 module Waffle
   class Configuration
 
     def initialize(custom_params = {})
-      @configuration = {'strategy' => 'rabbitmq', 'url' => nil}.merge custom_params
+      @configuration = {'strategy' => 'rabbitmq', 'url' => nil}
+
+      if defined? Rails
+        @configuration.merge! YAML.load_file("#{Rails.root}/config/#{Rails.env}.waffle.yml")
+      end
+
+      @configuration.merge! custom_params
     end
 
     def method_missing(m, *args, &block)
---
+++ test/test_utils.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require 'waffle/utils'
+
+class UtilsTest < Test::Unit::TestCase
+
+  def test_encode
+    encoded_msg = Waffle::Utils.encode({'a' => 1, 'b' => 2})
+    assert_equal encoded_msg, '{"a":1,"b":2}'
+  end
+
+  def test_decode
+    decoded_msg = Waffle::Utils.decode '{"a":1,"b":2}'
+    assert_equal decoded_msg, {'a' => 1, 'b' => 2}
+  end
+
+end
