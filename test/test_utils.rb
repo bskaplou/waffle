cb892f71b418a15b5ba365fa984161bc0a014672
Alexander Lomakin <alexander.lomakin@gmail.com>
2012-03-19 22:28:37 +0400

    Added brackets

--- test/test_utils.rb
+++ test/test_utils.rb
@@ -5,12 +5,12 @@ class UtilsTest < Test::Unit::TestCase
 
   def test_encode
     encoded_msg = Waffle::Utils.encode({'a' => 1, 'b' => 2})
-    assert_equal '{"a":1,"b":2}', encoded_msg 
+    assert_equal('{"a":1,"b":2}', encoded_msg)
   end
 
   def test_decode
-    decoded_msg = Waffle::Utils.decode '{"a":1,"b":2}'
-    assert_equal {'a' => 1, 'b' => 2}, decoded_msg
+    decoded_msg = Waffle::Utils.decode('{"a":1,"b":2}')
+    assert_equal({'a' => 1, 'b' => 2}, decoded_msg)
   end
 
 end
