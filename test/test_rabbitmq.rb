bbf4f7df9fdbe7df6187d2fa7b3db86d58a7a4c5
Alexander Lomakin <alexander.lomakin@gmail.com>
2012-03-21 20:23:58 +0400

    Added tests for RabbitMQ publish and subscribe

--- test/test_rabbitmq.rb
+++ test/test_rabbitmq.rb
@@ -1,5 +1,20 @@
 require 'test/unit'
+require 'mocha'
+require 'bunny'
 require 'waffle/transports/rabbitmq'
 
 class RabbitmqTest < Test::Unit::TestCase
+
+  def test_publish
+    Bunny.setup nil
+    Bunny::Exchange.any_instance.stubs(:publish).returns(nil)
+    assert_equal nil, Waffle::Transports::Rabbitmq.new.publish('events', 'message')
+  end
+
+  def test_subscribe
+    Bunny.setup nil
+    Bunny::Queue.any_instance.stubs(:subscribe).returns(nil)
+    assert_equal nil, Waffle::Transports::Rabbitmq.new.subscribe{ |m| puts m }
+  end
+
 end
