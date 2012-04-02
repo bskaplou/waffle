ecb09e728c3b3b5fa6d1e21dd86091b571e918e1
Alexander Lomakin <alexander.lomakin@gmail.com>
2012-04-02 23:17:51 +0400

    Yield routing_key to subscribe handler

--- lib/waffle/transports/rabbitmq.rb
+++ lib/waffle/transports/rabbitmq.rb
@@ -30,7 +30,7 @@ module Waffle
         end
 
         @queue.subscribe do |message|
-          yield encoder.decode(message[:payload])
+          yield message[:routing_key], encoder.decode(message[:payload])
         end
       end
 
