db0d107a9a38223b5458db311605f48254166c38
Alexander Lomakin <alexander.lomakin@gmail.com>
2012-04-05 18:35:52 +0400

    :Revert "Search waffle.yml in current dir"
    This reverts commit b35707a5d8eb0fb9e2fb88290b17fe4f7c5d945d.

--- lib/waffle/config.rb
+++ lib/waffle/config.rb
@@ -10,7 +10,7 @@ module Waffle
     def initialize
       @config_hash = {'transport' => 'rabbitmq', 'url' => nil, 'encoder' => 'json'}
 
-      filename = File.join Dir.pwd, "config/waffle.yml"
+      filename = "config/waffle.yml"
 
       if defined? Rails
         fielname = "#{Rails.root}/config/waffle.yml"
