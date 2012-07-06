643fa30611a42a492cda721c208f7d9ceaf19263
undr <undr@yandex.ru>
2012-07-06 13:11:52 +0700

    Fixed test. It was broken on linux ruby interpreters

--- spec/waffle/encoders/marshal_spec.rb
+++ spec/waffle/encoders/marshal_spec.rb
@@ -2,18 +2,16 @@ require 'spec_helper'
 
 describe Waffle::Encoders::Marshal do
   let(:message){{:data => 'message data', :occured_at => 'DateTime'}}
+  let(:marshalized_message){Marshal.dump(message)}
   describe '.encode' do
     specify do
-      Waffle::Encoders::Marshal.encode(message).should == 
-        "\x04\b{\a:\tdataI\"\x11message data\x06:\x06ET:\x0Foccured_atI\"\rDateTime\x06;\x06T"
+      Waffle::Encoders::Marshal.encode(message).should == marshalized_message
     end
   end
 
   describe '.decode' do
     specify do
-      Waffle::Encoders::Marshal.decode(
-        "\x04\b{\a:\tdataI\"\x11message data\x06:\x06ET:\x0Foccured_atI\"\rDateTime\x06;\x06T"
-      ).should == message
+      Waffle::Encoders::Marshal.decode(marshalized_message).should == message
     end
   end
 end
